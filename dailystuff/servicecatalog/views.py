7d5033df3e5ba6d32f48e08bfb02b7822d997535
Atul Singh <atul.nitr@gmail.com>
2019-12-23 23:16:23 +0530

    products by category

--- dailystuff/servicecatalog/views.py
+++ dailystuff/servicecatalog/views.py
@@ -6,6 +6,7 @@ from .models import ServiceCategory, ServiceProduct
 
 
 def allServiceProducts(request):
+    " Get All Service Products "
     service_list = ServiceProduct.objects.raw(
         'SELECT * FROM servicecatalog_serviceproduct where is_available=1'
     )
@@ -19,3 +20,21 @@ def allServiceProducts(request):
     except (EmptyPage, InvalidPage):
         services = paginator.page(paginator.num_pages)
     return render(request, 'servicecatalog/services.html', {'category': None, 'services': services})
+
+def serviceByCategory(request, s_url_key):
+    " Get Service Products by Service Category"
+    s_page = get_object_or_404(ServiceCategory, url_key=s_url_key)
+    service_list = ServiceProduct.objects.raw(
+        'SELECT * FROM servicecatalog_serviceproduct where is_available=1 '
+        'and servicecategory_id={}'.format(s_page.id)
+    )
+    paginator = Paginator(service_list, 6)
+    try:
+        page_num = int(request.GET.get('page', 1))
+    except:
+        page_num = 1
+    try:
+        services = paginator.page(page_num)
+    except (EmptyPage, InvalidPage):
+        services = paginator.page(paginator.num_pages)
+    return render(request, 'servicecatalog/services.html', {'category': s_page, 'services': services})
